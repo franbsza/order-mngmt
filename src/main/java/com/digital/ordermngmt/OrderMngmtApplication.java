42e74f780d41a89a0c127567c1fa20a8a1df8a19
franbsza <franciele.bsza@gmail.com>
2023-07-17 21:28:12 -0300

    init

---
+++ src/main/java/com/digital/ordermngmt/OrderMngmtApplication.java
@@ -0,0 +1,13 @@
+package com.digital.ordermngmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderMngmtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMngmtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/digital/ordermngmt/OrderMngmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.ordermngmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderMngmtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
